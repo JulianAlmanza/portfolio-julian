2e4a8bd15f83d89b1571eb0d33564ddf79ba45d3
Julian Almanza <deyvidjulian30@gmail.com>
2023-08-02 23:42:32 -0500

    Update projects

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Navbar() {
     return (
         <section className='flex w-full text-gray-500 p-5 bg-[#1f242d] justify-between nav sticky top-0'>
-            <a href="" className='text-lg text-green-400 2xl:text-5xl'>Julian Almanza Dev</a>
-            <nav className='flex text-xl gap-3 nav__links barra'>
+            <a href="" className='text-lg text-green-400 md:text-3xl  2xl:text-5xl'>Julian Almanza Dev</a>
+            <nav className='flex text-xl  gap-3 nav__links barra'>
                 <a href="#main" className='2xl:text-3xl nav__link navbar'>Home</a>
                 <a href="#about" className='2xl:text-3xl nav__link navbar'>About</a>
                 <a href="#services" className='2xl:text-3xl nav__link navbar'>Services</a>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Projects() {
   return (
     <section id='projects' className='bg-page2 h-[100vh]'>
-        <h2 className='text-3xl text-center'>Latest  <span className='text-span'>Project</span></h2>
+      <h2 className='text-3xl text-center'>Latest  <span className='text-span'>Project</span></h2>
+      <div className='flex w-[95%] m-auto gap-3 justify-center items-center '>
+        <div className='w-[820px] h-[400px] relative px-2 flex'>
+          <a href="https://modidecor-md-julianalmanza.vercel.app/" target='_blank'>
+            <Image src={'/image/projects/Captura 3.jpg'} alt='' layout='fill' className='object-cover w-[100%] h-[100%] flex' />
+          </a>
+
+        </div>
+        <p className='text-white text-2xl w-[735px]'>Web page for the company ModidecorMD dedicated to the manufacture of furniture. <span className='text-cyan-500'>Made with NextJS (React, Typescript, HTML5, CSS) </span></p>
+      </div>
     </section>
   )
 }
